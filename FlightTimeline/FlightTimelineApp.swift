1b7728934ec8a1ceb363c7ce62f3097784970adf
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-04-06 23:29:00 +0300

    Initial Commit

---
+++ FlightTimeline/FlightTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimelineApp.swift
+//  FlightTimeline
+//
+//  Created by Анатолий Силиверстов on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
